c8d91899312f51366051088708aeea57f402ce46
Louis Estrada <louestrada20@hotmail.com>
2025-10-25 00:54:50 -0500

    test: add intentional deployment failure for testing analysis system

---
+++ test-deployment-failure.js
@@ -0,0 +1,6 @@
+// This file contains an intentional syntax error to test deployment failure analysis
+// Delete this file after testing
+
+const testFunction = () => {
+  console.log("This will cause a build failure")
+  // Missing closing brace intentionally
