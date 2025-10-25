8c712e0d6dc5150125b0cd9497b78a62eae68f7b
Louis Estrada <louestrada20@hotmail.com>
2025-10-25 00:55:19 -0500

    cleanup: remove test deployment failure file

--- test-deployment-failure.js
+++
@@ -1,6 +0,0 @@
-// This file contains an intentional syntax error to test deployment failure analysis
-// Delete this file after testing
-
-const testFunction = () => {
-  console.log("This will cause a build failure")
-  // Missing closing brace intentionally
