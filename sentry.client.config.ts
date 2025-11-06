fda47753487d5481ce0f7685878a18c2322a0d50
Louis Estrada <louestrada20@hotmail.com>
2025-11-06 02:12:53 -0500

    fix: add missing sentry.client.config.ts for browser-side error tracking

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,5 +1,5 @@
-// This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
@@ -33,4 +33,3 @@ Sentry.init({
   sendDefaultPii: false,
 });
 
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
